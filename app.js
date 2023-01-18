2ee25285eb7c910458991a1ff4ebc4b1baaf33f4
Andrew Dasdfs <andrewdasdfs@gmail.com>
2023-01-18 15:22:12 +0200

    Решение дз "4-navigator"

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('Hello World!');
+const positionLat = 4;
+const positionLong = 3;
+const addressLat = 10;
+const addressLong = 9;
+
+console.log(Math.sqrt((addressLat - positionLat) ** 2 + (addressLong - positionLong) ** 2));
