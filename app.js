88b532c20f54099086d117f6189359f045af1ce9
Andrew Dasdfs <andrewdasdfs@gmail.com>
2023-01-20 15:25:47 +0200

    Решение дз "5-languages"

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-console.log('Hello World!');
+const lang = "en";
+
+switch (lang) {
+    case "en":
+        console.log("Good day!");
+        break;
+    case "ru":
+        console.log("Добрый день!");
+        break;
+    case "de":
+        console.log("Gutten tag!");
+        break;
+    default:
+        break;
+}
