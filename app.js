bac0c2406b91fd67bb2000e5267df498e328bcef
Andrew Dasdfs <andrewdasdfs@gmail.com>
2023-01-23 17:29:03 +0200

    Ререшие dz 6-driver

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-console.log('Hello World!');
+const hasLicence = true;
+const age = 16;
+const isDrink = false;
+
+const canDrive = age >= 16 && hasLicence && !isDrink;
+console.log(`Могу сесть за руль: ${canDrive ? 'Да' : 'Нет'}`);
