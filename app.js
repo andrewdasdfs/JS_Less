08eb21c2262d6c6c7da2ef48acb47345241cb684
Andrew Dasdfs <andrewdasdfs@gmail.com>
2023-01-25 16:48:57 +0200

    Решение дз "7-converter"

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Hello World!');
+function conv(rub, curr) {
+    const rate = {
+        usd: 0.014,
+        eur: 0.016
+    }
+    if (!rate[curr]) {
+        return null
+    } 
+        return rub * rate[curr] + [curr];
+}
+
+console.log(conv(15000, 'eur'));
